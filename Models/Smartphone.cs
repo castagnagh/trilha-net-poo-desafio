a740cfd85e1b0c85369a6637b939b82e32abb028
castagnagh <g.castagna.h@gmail.com>
2025-08-19 23:31:13 -0300

    feat: completando o desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private String Modelo { get; set; }
+        private String IMEI { get; set; }
+        private int Memoria { get; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "55999562352", modelo: "Modelo X", imei: "2345233462", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "55999181734", modelo: "Iphone 16e", imei: "2125678889765", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("GovBR");
